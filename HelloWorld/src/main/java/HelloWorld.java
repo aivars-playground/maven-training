e26201d08898133a6d94604df775f96b86db5c78
Aivars Smaukstelis <aivars.smaukstelis@gmail.com>
2020-12-10 15:24:56 +0200

    adding dependency

--- HelloWorld/src/main/java/HelloWorld.java
+++ HelloWorld/src/main/java/HelloWorld.java
@@ -1,5 +1,7 @@
+import org.apache.commons.lang3.text.WordUtils;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println(WordUtils.initials("Hello World"));
     }
 }
